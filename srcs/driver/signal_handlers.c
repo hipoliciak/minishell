05b27b62410f889e95df88d251bc63fd5f24df23
Dymirt <kolidadmytro@gmail.com>
2024-07-23 01:02:10 +0200

    fixing ctr+C issue
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- srcs/driver/signal_handlers.c
+++ srcs/driver/signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: dkolida <dkolida@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 19:21:53 by dkolida           #+#    #+#             */
-/*   Updated: 2024/07/21 19:41:12 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/07/23 00:56:18 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	sigint_handler(int sig_num)
 {
 	(void)sig_num;
+	rl_replace_line("\n", 0);
 	ft_putendl_fd("", 1);
 	rl_on_new_line();
 	rl_redisplay();
