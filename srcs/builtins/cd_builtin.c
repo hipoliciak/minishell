acba826256e7d9b62bd8ed32eb754b62e44fffbb
Dymirt <kolidadmytro@gmail.com>
2024-08-07 18:52:41 +0200

    Proposal for CD hendle 3 args
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- srcs/builtins/cd_builtin.c
+++ srcs/builtins/cd_builtin.c
@@ -6,11 +6,13 @@
 /*   By: dkolida <dkolida@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 23:02:48 by dmodrzej          #+#    #+#             */
-/*   Updated: 2024/08/01 15:45:44 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/08/07 18:50:47 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+//static char *handle_second_arg(char *path, char **args);
 
 static int	arg_error(t_shell *shell)
 {
@@ -40,7 +42,7 @@ int	cd_builtin(t_shell *shell, char **args)
 	char	*path;
 
 	path = "";
-	if (args[2])
+	if (args[3])
 		return (arg_error(shell));
 	else if (!args[1] || args[1][0] == '~')
 	{
@@ -62,3 +64,14 @@ int	cd_builtin(t_shell *shell, char **args)
 	free(path);
 	return (0);
 }
+
+// test in your terminal how command "cd $PWD {sone existing dir name}" works
+// if you agree add this function rigth after line 58 (chaking for path)
+/*
+static char *handle_second_arg(char *path, char **args)
+{
+	if (args[2])
+		path = ft_strjoin_and_free(path, ft_strjoin("/", args[2]));
+	return (path);
+}
+*/
