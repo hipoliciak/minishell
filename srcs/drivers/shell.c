f356de0c69ac37830b4acefa175a823eb7907793
Dymirt <kolidadmytro@gmail.com>
2024-08-27 14:56:29 +0200

    fixing norminette issue and refactoring
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- srcs/drivers/shell.c
+++ srcs/drivers/shell.c
@@ -6,13 +6,13 @@
 /*   By: dkolida <dkolida@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 21:58:23 by dmodrzej          #+#    #+#             */
-/*   Updated: 2024/08/26 23:56:21 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/08/27 14:54:43 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	shell_exec(t_shell *shell, char *line);
+void	shell_exec(t_shell *shell, char **tokens);
 void	pipe_exec(t_shell *shell);
 
 t_shell	*init_shell(void)
@@ -52,6 +52,7 @@ void	free_shell(t_shell *shell)
 int	run_shell(t_shell *shell)
 {
 	char	*line;
+	char	**tokens;
 
 	signal(SIGINT, sigint_handler);
 	line = NULL;
@@ -65,58 +66,31 @@ int	run_shell(t_shell *shell)
 			break ;
 		}
 		if (*line != '\0')
-			shell_exec(shell, line);
+		{
+			tokens = get_tokens(shell, line);
+			shell_exec(shell, tokens);
+		}
 	}
 	return (0);
 }
 
-void	shell_exec(t_shell *shell, char *line)
+void	shell_exec(t_shell *shell, char **tokens)
 {
-	char	**tokens;
 	int		pid;
 	int		pipefd[2];
-	char	*out_line;
-	int		in_fd;
 
-	tokens = get_tokens(shell, line);
 	if (tokens)
 	{
 		group_input(shell, tokens);
 		make_pipe(pipefd);
-		shell->group_i--;
 		pid = make_fork();
 		if (pid == 0)
-		{
-			if (shell->groups[0]->in_file_name)
-			{
-				in_fd = open(shell->groups[0]->in_file_name, O_RDONLY);
-				dup2(in_fd, STDIN_FILENO);
-				close(in_fd);
-			}
-			close(pipefd[0]);
-			dup2(pipefd[1], STDOUT_FILENO);
-			close(pipefd[1]);
-			pipe_exec(shell);
-			perror("pipe_execcc");
-			exit(EXIT_FAILURE);
-		}
+			child_process(shell, pipefd);
 		else
-		{
-			close(pipefd[1]);
-			out_line = get_next_line(pipefd[0]);
-			while (out_line != NULL)
-			{
-				printf("%s", out_line);
-				free(out_line);
-				out_line = get_next_line(pipefd[0]);
-			}
-			free(out_line);
-			close(pipefd[0]);
-			waitpid(pid, NULL, 0);
-		}
+			parent_process(pipefd, pid);
 		ft_free_split(tokens);
-		free_groups(shell->groups);
 	}
+	free_groups(shell->groups);
 }
 
 void	pipe_exec(t_shell *shell)
@@ -144,6 +118,7 @@ void	pipe_exec(t_shell *shell)
 			close(pipefd[0]);
 		}
 	}
-	//exec_command(shell, shell->groups[shell->group_i]->args);
 	execve_path(shell, shell->groups[shell->group_i]->args);
 }
+// To replace last line when buildins will be implemented with exit()
+//exec_command(shell, shell->groups[shell->group_i]->args);
--- srcs/parser/groups_init.c
+++ srcs/parser/groups_init.c
@@ -6,7 +6,7 @@
 /*   By: dkolida <dkolida@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 22:22:09 by dkolida           #+#    #+#             */
-/*   Updated: 2024/08/07 17:11:36 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/08/27 14:38:15 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,9 @@ void	group_input(t_shell *shell, char **tokens)
 			add_to_group(shell, tokens[i], i == tokens_c - 1);
 		i++;
 	}
+	(*group_i)--;
 }
+//The group_i variable is used to keep track of the current group index later.
 
 void	add_to_group(t_shell *shell, char *token, int is_end)
 {
