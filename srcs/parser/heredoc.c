43f5cb1784257309e398160752e24344f1d086e6
Dymirt <kolidadmytro@gmail.com>
2024-09-14 19:10:43 +0200

    fixing issues with externall functions
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- srcs/parser/heredoc.c
+++ srcs/parser/heredoc.c
@@ -6,47 +6,40 @@
 /*   By: dkolida <dkolida@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 17:26:28 by dkolida           #+#    #+#             */
-/*   Updated: 2024/09/14 17:44:15 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/09/14 19:09:13 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	child_heredoc_handler(int temp_fd, char *template, char *delimiter);
+void	child_heredoc_handler(char *template, char *delimiter);
 
 void	heredoc(t_shell *shell, t_tokenizer *data, int *grp_i, int *i)
 {
 	char	*delimiter;
 	char	*template;
-	int		temp_fd;
 	int		pid;
 
 	delimiter = data->tokens[++(*i)];
-	template = strdup("/tmp/heredocXXXXXX");
-	temp_fd = mkstemp(template);
-	if (temp_fd == -1)
-	{
-		perror("mkstemp");
-		return ;
-	}
+	template = NULL;
+	template = ft_strdup("minishell_heredoc");
 	pid = fork();
 	if (pid == 0)
-		child_heredoc_handler(temp_fd, template, delimiter);
+		child_heredoc_handler(template, delimiter);
 	else
 	{
 		wait(NULL);
 		shell->groups[*grp_i]->in_file_name = strdup(template);
-		close(temp_fd);
 	}
 	free(template);
 }
 
-void	child_heredoc_handler(int temp_fd, char *template, char *delimiter)
+void	child_heredoc_handler(char *template, char *delimiter)
 {
 	char	*line;
+	int		temp_fd;
 
-	close(temp_fd);
-	temp_fd = open(template, O_WRONLY | O_TRUNC);
+	temp_fd = open(template, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (temp_fd == -1)
 	{
 		perror("open");
